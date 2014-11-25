1fab74c9d45793999d211644a889497cec9ff36d
Gabor Bata <bata.gabor@gmail.com>
2014-11-25 12:06:16 +0100

    Made mouse positioning more accurate.

--- jsame4k-html5/optimization-phases/alternate_mouse_position_function.js
+++
@@ -1,8 +0,0 @@
-// based on the book: Core HTML5 Canvas by David Geary
-function getMousePosition(event) {
-    var boundingRect = canvas.getBoundingClientRect();
-    return {
-        x: (event.clientX - boundingRect.left) * (canvas.width / boundingRect.width),
-        y: (event.clientY - boundingRect.top) * (canvas.height / boundingRect.height)
-    };
-}
